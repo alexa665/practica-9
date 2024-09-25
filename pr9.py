53ed97c880af0fa09bc254bb7e4ba936e40b339e
alexa665 <a.23308051281205@cbtis128.edu.mx>
2024-09-24 20:46:16 -0600

    Add files via upload

---
+++ pr9.py
@@ -0,0 +1,12 @@
+print(" ")
+print("alexa guadalupe ramirez manzo") 
+print(" ") 
+#en este codigo se solisitaran tres numeros y se realizara un multiplicasin entre ellos
+x = int(input("ingresa un numero"))#variable y linea para ingresar el numero
+y = int(input("ingresa un numero")) #segunda variable y linea para ingresar el numero 
+z = int(input("ingresa un numero")) #tercera ariable y linea para ingresar el numero
+print(" ") #linea de espasio 
+mult = x*y*z #multiplicasion de las variables
+print(" ") #linea de espasio
+print ("el resultado de la multiplicasion es",mult) #inpresion de resultado 
+print(" ") #linea de espasio 
